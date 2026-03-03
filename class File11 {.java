4079b8b15263dff4a467f2c245280217ef0b559c
Dev? <ayanthakur.mumensom@outlook.com>
2026-03-03 16:29:34 +0100

    Add files via upload

---
+++ class File11 {.java
@@ -0,0 +1,18 @@
+class File11 {
+    public static void main(String args[]) {
+        boolean sunny = true;
+        boolean hot = false;
+
+        if (sunny) {
+            if (hot){
+                System.out.println("");
+
+            }else{
+                System.out.println("");
+            }
+        }else{
+            System.out.println("wdawd");
+
+        }
+    }
+}
